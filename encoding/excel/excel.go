7eeece42a21221e34d94f4bc70ca61edfea0db2b
shawnwyckoff <shawnwyckoff@protonmail.com>
2020-03-03 15:45:37 +0800

    add in memory excel doc functions

--- encoding/excel/excel.go
+++ encoding/excel/excel.go
@@ -5,44 +5,60 @@ import (
 	"bytes"
 	"fmt"
 	"github.com/extrame/xls"
+	"github.com/shawnwyckoff/commpkg/sys/fs"
 	"github.com/tealeg/xlsx"
+	"strconv"
+	"strings"
 )
 
-type ExcelDoc struct {
-	jsonstr string
-	xlsx    *xlsx.File
-	xls     *xls.WorkBook
+type (
+	XlsDoc struct {
+		xlsx *xlsx.File
+		xls  *xls.WorkBook
+	}
+
+	XlsSheet struct {
+		xlsxSheet *xlsx.Sheet
+		xlsSheet  *xls.WorkSheet
+	}
+
+	Col struct {
+		xlsxCol *xlsx.Col
+		xlsCol  *xls.Col
+	}
+
+	Row struct {
+		xlsxCol *xlsx.Row
+		xlsCol  *xls.Row
+	}
+
+	Cell struct {
+		xlsxCol *xlsx.Cell
+		xlsCol  *xls.CellRange
+	}
+
+	MemSheet struct {
+		Name    string
+		Content map[string]string // [2]uint32{RowId, CollId}"Cell Content"
+	}
+
+	MemDoc struct {
+		Sheets map[uint32]*MemSheet
+	}
+)
+
+func OpenPath(path string) (*XlsDoc, error) {
+	buf, err := fs.FileToBytes(path)
+	if err != nil {
+		return nil, err
+	}
+	return OpenBytes(buf)
 }
 
-type Sheet struct {
-	xlsxSheet *xlsx.Sheet
-	xlsSheet  *xls.WorkSheet
-}
-
-type Col struct {
-	xlsxCol *xlsx.Col
-	xlsCol  *xls.Col
-}
-
-type Row struct {
-	xlsxCol *xlsx.Row
-	xlsCol  *xls.Row
-}
-
-type Cell struct {
-	xlsxCol *xlsx.Cell
-	xlsCol  *xls.CellRange
-}
-
-func OpenPath(filename string) (*ExcelDoc, error) {
-	return nil, nil
-}
-
-func OpenBytes(b []byte) (*ExcelDoc, error) {
+func OpenBytes(b []byte) (*XlsDoc, error) {
 	dx, err := xlsx.OpenBinary(b)
 	if err == nil {
-		return &ExcelDoc{xlsx: dx}, nil
-
+		return &XlsDoc{xlsx: dx}, nil
 	}
 
 	reader, err := zip.NewReader(bytes.NewReader(b), int64(len(b)))
@@ -53,12 +69,11 @@ func OpenBytes(b []byte) (*ExcelDoc, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &ExcelDoc{xlsx: dx}, nil
-
+	return &XlsDoc{xlsx: dx}, nil
 }
 
-func (d *ExcelDoc) Sheets(idx int) *Sheet {
-	r := Sheet{}
+func (d *XlsDoc) Sheets(idx int) *XlsSheet {
+	r := XlsSheet{}
 	if d.xlsx != nil {
 		r.xlsxSheet = d.xlsx.Sheets[idx]
 	} else {
@@ -67,7 +82,15 @@ func (d *ExcelDoc) Sheets(idx int) *Sheet {
 	return &r
 }
 
-func (d *ExcelDoc) SheetCount() int {
+func (d *XlsDoc) SheetName(idx int) string {
+	if d.xlsx != nil {
+		return d.xlsx.Sheets[idx].Name
+	} else {
+		return d.xls.GetSheet(idx).Name
+	}
+}
+
+func (d *XlsDoc) SheetCount() int {
 	if d.xlsx != nil {
 		return len(d.xlsx.Sheets)
 	} else {
@@ -75,23 +98,23 @@ func (d *ExcelDoc) SheetCount() int {
 	}
 }
 
-func (d *ExcelDoc) RowCount(sheetIdx int) int {
+func (d *XlsDoc) RowCount(sheetIdx int) int {
 	if sheetIdx >= d.SheetCount() {
 		return 0
 	}
 
 	if d.xlsx != nil {
-		return len(d.xlsx.Sheets[sheetIdx].Rows)
+		return d.xlsx.Sheets[sheetIdx].MaxRow+1
 	} else {
 		return int(d.xls.GetSheet(sheetIdx).MaxRow) + 1
 	}
 }
 
-func (d *ExcelDoc) CellCount(sheetIdx, rowIdx int) int {
+func (d *XlsDoc) CollCount(sheetIdx, rowIdx int) int {
 	if sheetIdx >= d.SheetCount() {
 		return 0
 	}
-	if rowIdx >= d.RowCount(rowIdx) {
+	if rowIdx >= d.RowCount(sheetIdx) {
 		return 0
 	}
 
@@ -102,12 +125,11 @@ func (d *ExcelDoc) CellCount(sheetIdx, rowIdx int) int {
 	}
 }
 
-func (d *ExcelDoc) GetCell(sheetIdx, rowIdx, colIdx int) (string, bool) {
+func (d *XlsDoc) GetCell(sheetIdx, rowIdx, colIdx int) (string, bool) {
 	if sheetIdx < 0 || rowIdx < 0 || colIdx < 0 {
 		return "", false
 	}
-	if colIdx >= d.CellCount(sheetIdx, rowIdx) {
-		fmt.Println(":", d.CellCount(sheetIdx, rowIdx))
+	if colIdx >= d.CollCount(sheetIdx, rowIdx) {
 		return "", false
 	}
 	if d.xlsx != nil {
@@ -115,4 +137,117 @@ func (d *ExcelDoc) GetCell(sheetIdx, rowIdx, colIdx int) (string, bool) {
 	} else {
 		return d.xls.GetSheet(sheetIdx).Row(rowIdx).Col(colIdx), true
 	}
+}
+
+func (d *XlsDoc) ToMemDoc() *MemDoc {
+	res := &MemDoc{Sheets: map[uint32]*MemSheet{}}
+
+	sheetCount := d.SheetCount()
+	for sheetIdx := 0; sheetIdx < sheetCount; sheetIdx++ {
+		res.SetSheet(uint32(sheetIdx), d.SheetName(sheetIdx))
+		rowCount := d.RowCount(sheetIdx)
+		for rowId := 0; rowId < rowCount; rowId++ {
+			collCount := d.CollCount(sheetIdx, rowId)
+			for collIdx := 0; collIdx < collCount; collIdx++ {
+				cellStr, ok := d.GetCell(sheetIdx, rowId, collIdx)
+				if ok {
+					res.SetCell(uint32(sheetIdx), uint32(rowId), uint32(collIdx), cellStr)
+				}
+			}
+		}
+	}
+	return res
+}
+
+func (d *MemDoc) GetSheetName(idx uint32) string {
+	if _, ok := d.Sheets[idx]; !ok {
+		return ""
+	}
+	return d.Sheets[idx].Name
+}
+
+func (d *MemDoc) GetSheetCount() uint32 {
+	return uint32(len(d.Sheets))
+}
+
+func (d *MemDoc) GetRowCount(sheetIdx uint32) uint32 {
+	maxRowId := uint32(0)
+	for rowCollStr := range d.Sheets[sheetIdx].Content {
+		ss := strings.Split(rowCollStr, ",")
+		if len(ss) != 2 {
+			return 0
+		}
+		rowIdx, err := strconv.ParseInt(ss[0], 10, 64)
+		if err != nil {
+			return 0
+		}
+		if uint32(rowIdx) > maxRowId {
+			maxRowId = uint32(rowIdx)
+		}
+	}
+
+	return maxRowId+1
+}
+
+func (d *MemDoc) GetCollCount(sheetIdx uint32) uint32 {
+	maxCollId := uint32(0)
+	for rowCollStr := range d.Sheets[sheetIdx].Content {
+		ss := strings.Split(rowCollStr, ",")
+		if len(ss) != 2 {
+			return 0
+		}
+		colIdx, err := strconv.ParseInt(ss[1], 10, 64)
+		if err != nil {
+			return 0
+		}
+		if uint32(colIdx) > maxCollId {
+			maxCollId = uint32(colIdx)
+		}
+	}
+
+	return maxCollId+1
+}
+
+func (d *MemDoc) GetCell(sheetIdx, rowIdx, colIdx uint32) string {
+	return d.Sheets[sheetIdx].Content[fmt.Sprintf("%d,%d", rowIdx, colIdx)]
+}
+
+func (d *MemDoc) SetCell(sheetIdx, rowIdx, colIdx uint32, content string) {
+	_, ok := d.Sheets[sheetIdx]
+	if !ok {
+		d.Sheets[sheetIdx] = &MemSheet{Content: map[string]string{}}
+	}
+
+	d.Sheets[sheetIdx].Content[fmt.Sprintf("%d,%d", rowIdx, colIdx)] = content
+}
+
+func (d *MemDoc) SetSheet(sheetIdx uint32, name string) {
+	_, ok := d.Sheets[sheetIdx]
+	if !ok {
+		d.Sheets[sheetIdx] = &MemSheet{Content: map[string]string{}}
+	}
+	d.Sheets[sheetIdx].Name = name
+}
+
+func (d *MemDoc) ToXlsx() ([]byte, error) {
+	f := xlsx.NewFile()
+	sheetCount := d.GetSheetCount()
+	for sheetIdx := uint32(0); sheetIdx < sheetCount; sheetIdx++ {
+		f.AddSheet(d.GetSheetName(sheetIdx))
+		rowCount := d.GetRowCount(sheetIdx)
+		collCount := d.GetCollCount(sheetIdx)
+		for rowIdx := uint32(0); rowIdx < rowCount; rowIdx++ {
+			f.Sheets[sheetIdx].AddRow()
+			for collIdx := uint32(0); collIdx < collCount; collIdx++ {
+				f.Sheets[sheetIdx].Rows[rowIdx].AddCell()
+				s := d.GetCell(sheetIdx, rowIdx, collIdx)
+				f.Sheets[sheetIdx].Rows[rowIdx].Cells[collIdx].SetString(s)
+			}
+		}
+	}
+	res := bytes.NewBuffer(nil)
+	if err := f.Write(res); err != nil {
+		return nil, err
+	}
+	return res.Bytes(), nil
 }
---
+++ encoding/excel/excel_test.go
@@ -0,0 +1,20 @@
+package excel
+
+import (
+	"github.com/shawnwyckoff/commpkg/sys/fs"
+	"testing"
+)
+
+func TestMemDoc_ToXlsx(t *testing.T) {
+	s := "test.xlsx"
+	xd, err := OpenPath(s)
+	if err != nil {
+		t.Error(err)
+	}
+
+	b, err := xd.ToMemDoc().ToXlsx()
+	if err != nil {
+		t.Error(err)
+	}
+	fs.BytesToFile(b, s + "2.xlsx")
+}
