1ceaf666a55ce1e971ef726a16a30e911c3d7c47
shawnwyckoff <shawnwyckoff@protonmail.com>
2020-03-03 15:47:53 +0800

    refactor

--- net/htmls/goquery.go
+++ net/htmls/goquery.go
@@ -2,8 +2,12 @@ package htmls
 
 import (
 	"github.com/PuerkitoBio/goquery"
-	"io/ioutil"
 	"strings"
+)
+
+type (
+	SelectorBuilder struct{}
+	Doc goquery.Document
 )
 
 func NewDocFromHtmlSrc(htmlSrc *string) (*goquery.Document, error) {
@@ -13,21 +17,4 @@ func NewDocFromHtmlSrc(htmlSrc *string) (*goquery.Document, error) {
 		return nil, err
 	}
 	return doc, nil
-}
-
-func NewDocFromHtmlFile(filename string) (*goquery.Document, error) {
-	buf, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-	str := string(buf)
-	return NewDocFromHtmlSrc(&str)
-}
-
-func QueryStringElementWithOneOfClasses(element, oneOfClasses string) string {
-	return element + "." + oneOfClasses
-}
-
-func QueryStringElementWithClass(element, class string) string {
-	return element + "." + class
 }
