63f49985b910b3b1cbb763a9ba5664769f6be5fc
shawnwyckoff <shawnwyckoff@protonmail.com>
2020-03-17 12:22:05 +0800

    Update fileserver.go

--- net/ghttp/fileserver/fileserver.go
+++ net/ghttp/fileserver/fileserver.go
@@ -10,7 +10,7 @@ package fileserver
 import (
 	"expvar"
 	"github.com/pkg/errors"
-	"github.com/shawnwyckoff/gpkg/apputil/glogz"
+	"github.com/shawnwyckoff/gpkg/apputil/glog"
 	"github.com/valyala/fasthttp"
 	"github.com/valyala/fasthttp/expvarhandler"
 )
